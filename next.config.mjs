97809ca6ce9b68161430df05947d864bd674d2f6
Ojeh <ojehdayo@gmail.com>
2026-05-22 13:09:22 +0100

    FIX: webpack config error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,16 @@
 
 const nextConfig= {
     serverComponentsExternalPackages: ['@meshsdk/core', '@meshsdk/core-cst'],
+    turbopack: {},
     webpack: (config) => {
         config.experiments = {
             ...config.experiments,
             asyncWebAssembly: true,
         }
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: 'webassembly/async',
+        })
         return config
     }
 };
