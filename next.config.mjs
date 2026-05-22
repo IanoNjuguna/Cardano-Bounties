e5572d4bb0bc5177f916d4d7e84be2173a30f2cf
Ojeh <ojehdayo@gmail.com>
2026-05-22 12:29:06 +0100

    Deployment and witlist fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig= {
-    serverComponentsExternalPackages: ['@meshsdk/core', '@meshsdk/core-cst']
+    serverComponentsExternalPackages: ['@meshsdk/core', '@meshsdk/core-cst'],
+    webpack: (config) => {
+        config.experiments = {
+            ...config.experiments,
+            asyncWebAssembly: true,
+        }
+        return config
+    }
 };
 
 export default nextConfig;
