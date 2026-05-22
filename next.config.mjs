372ce176a43da9629c2c5d095b3c2250399d0b13
Ojeh <ojehdayo@gmail.com>
2026-05-22 13:05:53 +0100

    FIX: turbopack and webpack conflict fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig= {
-
-    // Fix build error
-    webpack: (config, { isServer }) => {
-        // Enable webAssembly experiments
+    serverExternalPackages: ['@meshsdk/core', '@meshsdk/core-csl'],
+    turbopack: {},
+    webpack: (config) => {
         config.experiments = {
             ...config.experiments,
             asyncWebAssembly: true,
-            layers: true,
-        };
-
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                fs: false,
-            };
         }
-
-        return config;
-    },
-    serverExternalPackages: ['@meshsdk/core', '@meshsdk/core-cst']
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: 'webassembly/async',
+        })
+        return config
+    }
 };
 
 export default nextConfig;
